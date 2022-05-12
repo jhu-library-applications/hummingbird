6e2469ac653320b8588ac7c30a19c51d047cbf2b
Jamie Little <jamie@jamielittle.org>
2022-05-12 11:43:30 -0500

    Add Codecov upload to GitHub Actions

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,6 +11,9 @@ require 'rspec/rails'
 require 'simplecov'
 SimpleCov.start
 
+require 'simplecov-cobertura'
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
