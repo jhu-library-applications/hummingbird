c917c1047f19593919bbe85639f0a411db413991
Stephen <stephen@MSEL-LIAPMAC07.local>
2022-06-02 12:15:26 -0400

    Add fixes for rubocop offenses

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
+# this class adds cancancan abilities for users based on their roles
 class Ability
   include CanCan::Ability
 
   def initialize(user)
-
     can :read, Vendor
 
-    return unless user.present?
+    return if user.blank?
 
-    can [:update, :create, :destroy], Vendor
+    can %i[update create destroy], Vendor
   end
 end
