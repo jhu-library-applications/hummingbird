cdcf4f7927dbc8aa5f21fa497062d025b146aa76
Stephen <stephen@MSEL-LIAPMAC07.local>
2022-06-02 17:15:49 -0400

    fix rubocop violations

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,14 +5,16 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-
     return if user.blank?
+
     can :read, Vendor
 
     return if user.editor
+
     can %i[update create], Vendor
 
-    return if !user.admin
+    return unless user.admin
+
     can %i[update create destroy], Vendor
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
+# this class creates an object that enables people to use the site in an
+# authoritative fashion
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :role, inclusion: { in: %w(reader editor admin) }
+  validates :role, inclusion: { in: %w[reader editor admin] }
 
-  %w(reader editor admin).each do |a|
+  %w[reader editor admin].each do |a|
     define_method(a) do
-      self.role == a
+      role == a
     end
   end
 end
