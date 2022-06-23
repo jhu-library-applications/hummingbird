86d7c2e9e7eb682ccdb5fbc4ee6a195533a8a6a6
Jamie Little <jamie@jamielittle.org>
2022-06-23 16:07:46 -0500

    Import vendors from CSV (#16)
    This commit will allow you to import vendors using the `rails db:seed` command.
    
    The script removes all the existing vendors before reading the CSV and seeding the db.
    
    Additional data import should be done in that script.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
+
+require 'csv'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +9,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Make this idempotent
+
+# DatabaseHeading.destroy_all
+# Database.destroy_all
+# Heading.destroy_all
+# Subheading.destroy_all
+Vendor.destroy_all
+# Database.destroy_all
+
+# Create vendors
+
+vendor_headers = %i[ID vendor]
+CSV.foreach(Rails.root.join('test_data/tbl_dict_vendors.txt'), headers: vendor_headers) do |row|
+  next if row[:ID] == 'ID'
+
+  Vendor.new(id: row[:ID], brand_name: row[:vendor]).save!
+end
